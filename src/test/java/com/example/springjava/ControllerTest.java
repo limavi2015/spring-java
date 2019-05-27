fe21638a53fc3282f5144599f3d23d746d7f4172
lmanjarres <lmanjarres@stratio.com>
2019-05-27 15:01:17 -0500

    se agregan pruebas unitarias con una base de datos embebida h2

--- src/test/java/com/example/springjava/ControllerTest.java
+++ src/test/java/com/example/springjava/ControllerTest.java
@@ -1,29 +1,30 @@
-/*package com.example.springjava;
+package com.example.springjava;
 
-import org.flywaydb.test.FlywayTestExecutionListener;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureTestDatabase
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, FlywayTestExecutionListener.class})
-@ActiveProfiles("test")
-@SpringBootTest(classes = Application.class)
 public class ControllerTest {
 
     private static final String URL_SERVICE = "/api/v1/products";
@@ -43,8 +44,41 @@ public class ControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect( status().isOk() )
                 .andExpect(jsonPath("$.length()", is(4)))
-                .andExpect(jsonPath("$.campaigns.[0].takerDocumentType", is("1")));
+                .andExpect(jsonPath("$.[0].id", is("1")))
+                .andExpect(jsonPath("$.[0].name", is("galletas")))
+                .andExpect(jsonPath("$.[0].value", is(2000)));
+    }
+
+    @Test
+    public void test2ShouldSaveAProductOK() throws Exception {
+        String product_json = "{\n" +
+                "    \"id\": \"5\",\n" +
+                "    \"name\": \"Quesito\",\n" +
+                "    \"value\": 3500\n" +
+                "}";
+
+        mockMvc.perform(post( URL_SERVICE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(product_json)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Producto guardado sarisfactoriamente"));
+    }
+
+
+    @Test
+    public void test3ShouldFailToSaveAProduct() throws Exception {
+        String product_json = "{\n" +
+                "    \"id\": \"5\",\n" +
+                "    \"name\": \"Quesito\",\n" +
+                "    \"value\": \"Quesito\",\n" +
+                "}";
+
+        mockMvc.perform(post( URL_SERVICE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(product_json)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 
 }
-*/
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,6 +1,3 @@
---CREATE USER postgres WITH ENCRYPTED PASSWORD 'mysecretpassword' NOCREATEDB;
---CREATE DATABASE public WITH OWNER postgres;
-
 CREATE TABLE IF NOT EXISTS public.productos
 (
    identificador             	text,
@@ -8,7 +5,8 @@ CREATE TABLE IF NOT EXISTS public.productos
    valor                       	text,
    CONSTRAINT pk_producto PRIMARY KEY (identificador)
 );
-insert into public.productos values ('1','galletas', 2000);
+
+insert into productos values ('1','galletas', 2000);
 insert into public.productos values ('2','chocolate', 6000);
 insert into public.productos values ('3','queso', 5000);
 insert into public.productos values ('4','pan', 1000);
